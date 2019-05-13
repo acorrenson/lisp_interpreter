827d5c06c475888c11f17d149c30c6094450100f
Arthur Correnson <arthur.correnson@gmail.com>
2019-05-13 12:23:18 +0200

    Improve pp_ast function

--- src/ast.ml
+++ src/ast.ml
@@ -12,15 +12,15 @@ type ast =
     let rec prec ast i =
       let rec pp_list l = 
         match l with
-        | [] -> print_newline ()
+        | [] -> ()
         | a::tail -> prec a (i+1); pp_list tail
       in
       match ast with
-      | End -> print_endline "End"
-      | String s -> print_endline ((String.make i ' ')^s)
-      | Var s -> print_endline ((String.make i ' ')^s)
-      | Num n -> print_endline ((String.make i ' ')^(string_of_int n))
-      | Call (s, l) -> print_endline ((String.make i ' ')^s); pp_list l
-      | List l -> print_endline "List"; pp_list l
+      | End         -> print_endline "End"
+      | String s    -> print_endline ((String.make (2*i) ' ')^s)
+      | Var s       -> print_endline ((String.make (2*i) ' ')^s)
+      | Num n       -> print_endline ((String.make (2*i) ' ')^(string_of_int n))
+      | Call (s, l) -> print_endline ((String.make (2*i) ' ')^s); pp_list l
+      | List l      -> print_endline "List"; pp_list l
     in
     prec ast 0

c53ce3085f382bda36dc50fcde58eb92094c64dd
Arthur Correnson <arthur.correnson@gmail.com>
2019-06-22 16:56:44 +0200

    Fix sub operation

--- _build/default/main.ml
+++ _build/default/main.ml
@@ -5,8 +5,7 @@ let _ =
     let lexbuf = Lexing.from_channel stdin in
     while true do
       let r = Parser.main Lexer.token lexbuf in
-        psexpr r; print_newline();
-        ppast (eval r); flush stdout
+        ppast (eval r); print_newline (); flush stdout
     done
   with Lexer.Eof ->
     exit 0
--- _build/default/src/ast.ml
+++ _build/default/src/ast.ml
@@ -8,6 +8,20 @@ type sexpr =
   | Pair of sexpr * sexpr
   | Nil
 
+let ppast a =
+  match a with
+  | Num n -> print_endline ("- int : " ^ (string_of_int n))
+  | Sym s -> print_endline ("- sym : " ^ s)
+  | _ -> print_endline ("- list")
+
+
+let rec list_of_sexpr s =
+  match s with
+  | Nil -> []
+  | Pair (a, b) -> ppast a; a::(list_of_sexpr b)
+  | _ -> failwith "this sexpr isn't a list"
+
+
 let rec psexpr s =
   match s with
   | Pair (s1, s2) ->
@@ -26,6 +40,7 @@ let rec eval sxp =
   | Sym _ -> sxp
   | Nil -> Nil
   | Pair (Sym s, body) ->
+    psexpr (eval_list body); print_newline();
     begin
       match s with
       | "/"   -> divide (eval_list body) (* env *)
@@ -44,8 +59,8 @@ and eval_list l =
   | Num n -> Num n
   | Sym s -> Sym s
   | Pair (Sym _, _) -> eval l
-  | Pair (s, Nil) -> Pair (eval_list s, Nil)
-  | Pair (s1, s2) -> Pair (eval_list s1, eval_list s2)
+  | Pair (s, Nil) -> Pair (eval s, Nil)
+  | Pair (s1, s2) -> Pair (eval s1, eval_list s2)
   | Nil -> Nil
 
 and divide l =
@@ -55,6 +70,7 @@ and divide l =
 
 and add l =
   match l with
+  | Pair (Num a, Nil) -> Num a
   | Pair (Num a, Pair (Num b, Nil)) -> Num (a + b)
   | Pair (Num a, Pair (Num b, s)) -> add (Pair ( Num a, add (Pair (Num b, s))))
   | Pair (Num a, Num b) -> Num (a + b)
@@ -62,16 +78,21 @@ and add l =
 
 and mult l =
   match l with
-  | Pair (Num a, Pair (Num b, Nil)) -> Num (a * b)
   | Pair (_, Nil) -> failwith "single operand for mult forbiden"
-  | Pair (Num a, s) -> mult (Pair (Num a, mult s))
+  | Pair (Num a, Pair (Num b, Nil)) -> Num (a * b)
+  | Pair (Num a, Pair (Num b, s)) -> mult (Pair ( Num a, mult (Pair (Num b, s))))
+  | Pair (Num a, Num b) -> Num (a * b)
   | _ -> failwith "operand error"
 
 and sub l =
   match l with
   | Pair (Num a, Nil) -> Num (- a)
-  | Pair (Num a, Pair (Num b, Nil)) -> Num (a - b)
-  | Pair (Num a, b) -> sub (Pair (Num a, sub b))
+  | Pair (Num a, r) ->
+    List.fold_left (
+      fun a b -> match a, b with
+      | Num a, Num b  -> Num (a - b)
+      | Num a, Nil    -> Num a
+      | _ -> failwith "operand error") (Num a) (list_of_sexpr r)
   | _ -> failwith "operand error"
 
 and incr l =
@@ -99,8 +120,3 @@ and cond l =
 (* Printing                     *)
 (* ============================ *)
 
-let ppast a =
-  match a with
-  | Num n -> print_endline ("- int : " ^ (string_of_int n))
-  | Sym s -> print_endline ("- sym : " ^ s)
-  | _ -> print_endline ("- list")
